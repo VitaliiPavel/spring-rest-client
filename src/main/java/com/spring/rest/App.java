db4fd075e5a8aaeefb02c5df4de24e23d58cd94d
VitaliiPavel <vitaliigherta@gmail.com>
2023-02-24 11:33:56 +0200

    added tests

--- src/main/java/com/spring/rest/App.java
+++ src/main/java/com/spring/rest/App.java
@@ -1,16 +1,11 @@
 package com.spring.rest;
 
-import com.spring.rest.configuration.MyConfig;
-import org.springframework.context.annotation.AnnotationConfigApplicationContext;
-import org.springframework.http.ResponseEntity;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+@SpringBootApplication
 public class App {
     public static void main(String[] args) {
-        AnnotationConfigApplicationContext context =
-                new AnnotationConfigApplicationContext(MyConfig.class);
-        Communication communication = context.getBean("communication", Communication.class);
-        ResponseEntity<String> response = communication.registerSmsTransaction(
-                400, 498, "127.0.0.1", "en", "PostMan");
-        System.out.println("test");
+        SpringApplication.run(App.class, args);
     }
 }
---
+++ src/test/java/com/spring/rest/CommunicationTest.java
@@ -0,0 +1,102 @@
+package com.spring.rest;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.test.context.junit4.SpringRunner;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CommunicationTest {
+
+    public final int AMOUNT = 400;
+    public final int CURRENCY = 498;
+    public final String CLIENT_IP_ADR = "127.0.0.1";
+    public final String LANGUAGE = "en";
+    public final String DESCRIPTION = "PostMan";
+    public final String TRANS_ID = "nOq6cnVflSX3ZmYLK42LbOEGHo0=";
+
+    @Autowired
+    private Communication communication;
+
+    @Test
+    public void registerSmsTransactionTest() throws JsonProcessingException {
+        ResponseEntity<String> response = communication.registerSmsTransaction(
+                AMOUNT, CURRENCY, CLIENT_IP_ADR, LANGUAGE, DESCRIPTION);
+
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    @Test
+    public void getTransactionResultTest() {
+        ResponseEntity<String> response = communication.getTransactionResult(TRANS_ID, CLIENT_IP_ADR);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    @Test
+    public void registerDmsTransactionTest() {
+        ResponseEntity<String> response = communication.registerDmsTransaction(
+                CLIENT_IP_ADR, AMOUNT, CURRENCY, DESCRIPTION, LANGUAGE);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    @Test
+    public void makeDmsTransactionTest() {
+        ResponseEntity<String> response = communication.makeDmsTransaction(
+                CLIENT_IP_ADR, TRANS_ID, AMOUNT, CURRENCY, DESCRIPTION, LANGUAGE);
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    @Test
+    public void revertTransactionTest() {
+        ResponseEntity<String> response = communication.revertTransaction(TRANS_ID, AMOUNT, "");
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    @Test
+    public void closeDayTest() {
+        ResponseEntity<String> response = communication.closeDay();
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    @Test
+    public void registerRegularSmsTransactionTest() {
+        ResponseEntity<String> response = communication.registerRegularSmsTransaction(
+                AMOUNT, CURRENCY, CLIENT_IP_ADR, LANGUAGE, DESCRIPTION, "", "", "", "");
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    @Test
+    public void registerRegularDmsTransactionTest() {
+        ResponseEntity<String> response = communication.registerRegularDmsTransaction(
+                AMOUNT, CURRENCY, CLIENT_IP_ADR, LANGUAGE, DESCRIPTION, "", "", "", "");
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    @Test
+    public void registerRegularTransactionTest() {
+        ResponseEntity<String> response = communication.registerRegularTransaction(
+                AMOUNT, CURRENCY, CLIENT_IP_ADR, LANGUAGE, DESCRIPTION, "", "", "");
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    @Test
+    public void makeTransactionTest() {
+        ResponseEntity<String> response = communication.makeTransaction(
+                AMOUNT, CURRENCY, CLIENT_IP_ADR, LANGUAGE, DESCRIPTION, "");
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+    @Test
+    public void deleteTransactionTest() {
+        ResponseEntity<String> response = communication.deleteTransaction("");
+        assertEquals(HttpStatus.OK, response.getStatusCode());
+    }
+
+}
